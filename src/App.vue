0f4abcca1caf415aae0aa6a1f3105783eded1b98
002 IT Chananan <namayura47@gmail.com>
2024-07-02 13:57:52 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome to Deploying a Vue.js with Docker for Development</h1>
+    <br />
+    <h2>My name is: {{ name }}</h2>
+    <h2>Ny college is:{{ college }}</h2>
+  </div>
+</template>
+
+<script setup>
+  const name ="Mr.Park hyongsok"
+  const college ="โรงเรียนไสยเวท"
+</script>
+
+<style lang="scss" scoped>
+
+</style>
